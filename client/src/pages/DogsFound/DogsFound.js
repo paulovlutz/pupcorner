a74ab0227e3f7d0717541c676f7b26f86431eab0
Paulo <paulovlutz@gmail.com>
2020-06-24 14:23:40 -0400

    change homepage image size and titles on the dogsfound page

--- client/src/pages/DogsFound/DogsFound.js
+++ client/src/pages/DogsFound/DogsFound.js
@@ -13,14 +13,14 @@ class HomePage extends Component {
                 <Header />
                 <section className="dogsfound">
                     <div className="dogsfound__closest">
-                        <h1 className="dogsfound__title">Best Close Matches</h1>
+                        <h1 className="dogsfound__title">Best Matches</h1>
                         <Row className="justify-content-lg-center">
                             <CardDogFound dogs={this.props.location.state.dogs} />
                         </Row>
                     </div>
 
                     <div className="dogsfound__others">
-                        <h1 className="dogsfound__title">Other Dogs Found</h1>
+                        <h1 className="dogsfound__title">Other Dogs</h1>
                         <Row className="justify-content-lg-center">
                             <CardDogFound dogs={this.props.location.state.otherDogsFound}/>
                         </Row>
