c2540017bed6d743bb2aa9fca51cd995a6cbc728
Paulo <paulovlutz@gmail.com>
2020-06-25 10:44:50 -0400

    add contact information on the about page for presentation purposes

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -16,6 +16,8 @@ const About = () => {
                     <h1 className="about__text-title">Pupcorner</h1>
                     <p className="about__text-slogan">Helping you find the pawfect friend in this pupcorner of the world!</p>
                     <p className="about__text-description">This Web Application was created as my Capstone Project in the Web Development Program (Spring Cohort 2020) at <a className="about__text-brainstation" href="https://brainstation.io/" target="_blank" rel="noopener noreferrer">BrainStation</a>.</p>
+                    <p className="about__text-contact">If you would like to know a little more about the project, feel free to contact me. I would love to answer any of your questions.</p>
+                    <p><a className="about__text-linkedin" href="https://www.linkedin.com/in/paulo-vinicius-lutz/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i> linkedin.com/in/paulo-vinicius-lutz/</a></p>
                 </div>
                 <AnimatedSquareBackground />
             </section>
